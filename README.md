fd613e57c7d991747541937916c5917d775dc405
Savio Monteiro <savimonty@gmail.com>
2018-10-06 19:16:06 -0500

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# gglDynNodeCron
+Created this simple tool in NodeJS to update my google domain using a cron job.
+# ggl-domain-updater-node
---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const request = require('request');
+
+const GOOGLE_DYNDNS_USERNAME = process.env.GOOGLE_DYNDNS_USERNAME;
+const GOOGLE_DYNDNS_PASSWORD = process.env.GOOGLE_DYNDNS_PASSWORD;
+const GOOGLE_FQDN = process.env.GOOGLE_DYNDNS_FQDN;
+
+const googleDomainAuth = Buffer.from(GOOGLE_DYNDNS_USERNAME + ":" + GOOGLE_DYNDNS_PASSWORD).toString('base64');
+
+function updateGoogleDomains() {
+    console.log(GOOGLE_DYNDNS_USERNAME)
+
+    var form = {
+        hostname: GOOGLE_FQDN
+    };
+
+    post({
+        headers: {
+            "Authorization": "Basic " + googleDomainAuth,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: "https://domains.google.com/nic/update",
+        form: form
+      }, function (error, response, body) {
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', body);
+      });
+}
+
+updateGoogleDomains();
